592620956cf241b03b69a9cb5d18561d2bea6f41
Vedant Bhosale <vedantbhosale001@gmail.com>
2025-07-05 12:07:22 +0100

    updates

--- Agent/Agentic_workflow.py
+++ Agent/Agentic_workflow.py
@@ -8,14 +8,40 @@ from tools.place_search_tool import PlaceSearchTool
 from tools.expense_calculator_tool import CalculatorTool
 from tools.currency_conversion_tool import CurrencyConverterTool
 
+
+
+
+
 class GraphBuilder():
     def __init__(self,graph):
-        self.graph = graph
+        self.tools=[
 
 
-    def agent_function(self):
-        pass
+
+        ]
+        self.system_prompt=SYSTEM_PROMPT
+
+
+    def agent_function(self,state: MessagesState):
+        """Main Afent function that processes user messages and invokes the LLM with tools."""
+        user_quesion=state["messages"]
+        input_question= [self.system_prompt] + user_quesion
+        response=self.llm_with_tools.invoke(input_question)
+        return {"messages":[response]}
 
 
     def build_graph(self):
-        pass
+        graph_builder=StateGraph(MessagesState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent",tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+        self.graph = graph_builder.compile()
+        return self.graph
+       
+
+    def __call__(self, *args, **kwds):
+        return self.build_graph()
+
--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
+from agent.agentic_workflow import GraphBuilder 
+from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+
+
+app=FastAPI()
+
+
+class QueryRequest(BaseModel):
+    query:str
     
+@app.post("/query"):
+
+async def query_travel_agent(query: QueryRequest):
+    try:
+        graph=GraphBuilder(model_provider="groq")
+        react_graph=graph()
